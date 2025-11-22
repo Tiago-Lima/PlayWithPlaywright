e908fe342b522ba963979fd0a88fe8ea52d95aec
Tiago Lima <tiago.santoslima91@gmail.com>
2025-11-22 12:20:34 -0300

    Validating products list

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -14,19 +14,28 @@ export class HomePage {
     //Carrousel indicators
     readonly indicators: Locator;
     readonly activeSlide: Locator;
-
+    //Featured Items
+    readonly featuredItensTitle: Locator;
+    readonly featuredItensProducts: Locator;
+    readonly featuredItensOverlay: Locator;
+    readonly featuredItensAddToCartButton: Locator;
 
     constructor(page: Page) {   
     this.page = page;
+    //Header constructor
     this.logo = page.locator('.logo.pull-left a');
     this.menuItens = page.locator('.fa fa-home');
-
+    //Carrousel constructor
     this.carrousel = page.locator('#slider-carousel');
       this.previousButton = this.carrousel.locator('a[data-slide="prev"]');
       this.nextButton = this.carrousel.locator('a[data-slide="next"]');
       this.indicators = this.carrousel.locator('.carousel-indicators > li');
       this.activeSlide = this.carrousel.locator('.item.active');
-      
+    //Featured Items constructor
+    this.featuredItensTitle = page.locator('.features_items h2.title');
+    this.featuredItensProducts = page.locator('.features_items .product-image-wrapper');
+    this.featuredItensOverlay = page.locator('.features_items .product-overlay');
+    this.featuredItensAddToCartButton = this.featuredItensOverlay.locator('.add-to-cart');
 
     }
 
@@ -88,6 +97,10 @@ export class HomePage {
        * 
        * @returns the the Test case button Locator  of the active slide
        */
+      async getCarrouselSlidesCount(): Promise<number> {
+        return await this.indicators.count();
+      }
+ 
       getCarrouselButtonTestCase() {
       return this.activeSlide.locator('.test_cases_list');
       }
@@ -115,7 +128,65 @@ export class HomePage {
       async getMenuItem(i: number) {
         return this.menuItens.nth(i);
       }
+    
+      //********************************************* Featured Itens methods ***********************************************
+      /**
+       * 
+       * @returns number of products in Featured Items section
+       */
+      async getFeaturedItemsProductsCount(): Promise<number> {
+        return await this.featuredItensProducts.count();
+      } 
+      
+      /**
+       * 
+       * @param index index of product in Featured Items section
+       * @returns Locator of product at index in Featured Items section
+       */
+         getFeaturedItemProduct(index: number): Locator {
+        return this.featuredItensProducts.nth(index);
+      }
 
+      /**
+       * 
+       * @param index index of product in Featured Items section
+       * Hovers the product at index in Featured Items section
+       */
+      async hoverFeaturedItemProduct(index: number): Promise<void> {
+        const product = await this.getFeaturedItemProduct(index);
+        await product.hover();
+      }
+
+      /**
+       * 
+       * @param index index of product in Featured Items section
+       * @returns Locator of overlay of product at index in Featured Items section
+       */
+         getFeaturedItemOverlay(index: number): Locator  {
+        return this.featuredItensOverlay.nth(index);
+      }
+      /**
+       * 
+       * @param index index of product in Featured Items section
+       * Waits for overlay of product at index in Featured Items section to be visible
+       */
+      async waitForOverlayVisible(index: number): Promise<void> {
+        const overlay = await this.getFeaturedItemOverlay(index);
+        await overlay.waitFor({ state: 'visible' });
+      }
+      
+      /**
+       * 
+       * @param index index of product in Featured Items section
+       * @returns Locator of Add to Cart button of product at index in Featured Items section
+       */
+       getFeaturedItemAddToCartButton(index: number): Locator { 
+        const overlay =  this.getFeaturedItemOverlay(index);
+        return overlay.locator('.add-to-cart');
+      }
+         
+
+      
 
     
   }
